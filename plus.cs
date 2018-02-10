cc3a0f1e70b0dcf6a94cda4361dce7931999749a
JustLabs445 <36343486+JustLabs445@users.noreply.github.com>
2018-02-10 16:35:50 -0500

    Update plus.cs

--- plus.cs
+++ plus.cs
@@ -1,5 +1,25 @@
 using System;
 
 class Plus {
-
+  string name = "files";
+  
+  public static void Plus(string[] args) {
+    bool good = CheckLength();
+    
+    if(good) {
+      Console.WriteLine("Your name's length is " + name.Length + ".");
+    }
+    else {
+      Console.WriteLine("Too short!");
+    }
+  }
+  
+  bool CheckLength() {
+    if(name.Length >= 5) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
